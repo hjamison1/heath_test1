d457379a1e0aa84342b1d556253f83d6fca3f5f4
hjamison1 <heathjamison@gmail.com>
2023-02-12 19:51:41 -0600

    test 1 7pm

--- heath_test1/Program.cs
+++ heath_test1/Program.cs
@@ -1,3 +1,10 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("GitHub test 6fortyfivepm");
+
+Console.WriteLine("backup working");
+
+Console.WriteLine("BitHub");
+
